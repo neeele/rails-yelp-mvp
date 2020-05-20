f24a1f07f644ea811f72d1281f7c033e51e274ef
Cornelia <neele@protonmail.com>
2020-05-20 16:35:42 +0200

    maybe to be continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "controller#action"
-  resources :restaurants
-# resources :reviews
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+  end
+
+  resources :reviews, only: [ :destroy ]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+langhans_grill = { name: "Rambo Doener", address: "Berliner Allee. 43", phone_number:"030 345 32 21", category: "belgian", rating: 5 }
+kathmandu = { name: "Rambo Doener", address: "Berliner Allee. 43", phone_number:"030 345 32 21", category: "belgian", rating: 5 }
+sushi_susi = { name: "Sushi Susi", address: "Langhansstr. 43", phone_number:"030 345 32 21", category: "italian", rating: 5 }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE",phone_number:"030 345 32 21", rating: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ",phone_number:"030 345 32 21", rating: 4 }
+[ rambo_doener, sushi_susi, dishoom, pizza_east, kathmandu, langhans_grill ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
